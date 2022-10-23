b375769d94217b0361ff18133fed61793bb186ba
roxannerbr <romaroxana876@gmail.com>
2022-10-23 19:15:33 -0300

    t.p vistas dinamicas

---
+++ src/controllers/indexController.js
@@ -0,0 +1,24 @@
+let menu = require ('../data/menu.json')
+
+module.exports = {
+    home: (req, res) => {
+
+        return res.render ('index', {
+            menu
+        })
+    },
+    menu: (req,res) => {
+        return res.render ('menu', {
+            menu
+        })
+    },
+    detalle: (req,res) => {
+        let id = +req.params.id
+
+       let plato = menu.find( plato => plato.id === id)
+        return res.render ('detalleMenu', {
+
+        plato})
+    }
+}
+
